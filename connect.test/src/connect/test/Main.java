f1917018ea9dc829c42c3e29c0338a02b5deed5b
SeungJae <sjung1126@naver.com>
2020-04-08 02:16:23 +0900

    DB_CONNECTED(p)
    practice for first time

---
+++ connect.test/src/connect/test/Main.java
@@ -0,0 +1,10 @@
+package connect.test;
+
+public class Main {
+
+		public static void main(String[] args) {
+			
+			DBConnection connection = new DBConnection();
+			System.out.println("관리자 여부 : " + connection.isAdmin("admin","admin"));
+		}
+}
